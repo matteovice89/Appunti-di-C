b44e11d91b86a36d95429e7ece520c26bc536132
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-10-29 16:27:50 +0100

    Create funzioni.c

---
+++ funzioni.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*FUNZIONI
+
+Una definizione è costituita da due parti:
+
+    -la dichiarazione della funzione
+    -il corpo della funzione, racchiuso tra parentesi graffe e comprendente zero o più di queste componenti:
+        *dichiarazioni e definizioni di variabili
+        *istruzioni
+        *istruzione return
+la prima parte è la dichiarazine che devve conternere:
+
+    il tipo ritornato
+    il nome della funzione
+    l'elenco dei parametri (argomenti)
+
+/* esempio di definizione */
+int secondi(int h, int m, int s) {/* in fase di dichiarazione è possibile omettere il nome dei parametri */
+    return (3600 * h + 60 * m + s);
+    }
+/*invocazione
+Una funzione termina quando: (a) viene eseguita l'istruzione return, oppure (b) viene eseguita l'ultima istruzione.*/
+int main() {
+    int h=1, m=1, s=1, totale_secondi;
+    totale_secondi = secondi(h, m, s);
+    printf("Totale secondi: %d\n", totale_secondi);
+}
+
