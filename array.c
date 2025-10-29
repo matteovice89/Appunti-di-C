9f9ad437a6f4317cea3a5dfbf12eb13fc2802ec4
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-10-29 12:21:07 +0100

    Enhance array.c with comments and ARRAY_SIZE macro

--- array.c
+++ array.c
@@ -2,19 +2,28 @@
 
 int main()
 {
-    /*definizione*/
-    double numbers[50]; /*posso definirli con tipo nome [cardinalità]*/
-    int dxm[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /*non indico cardinalità ma espl
+	/*definizione*/
+	double numbers[50]; /*posso definirli con tipo nome [cardinalitC ]*/
+	int dxm[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /*non indico cardinalitC  ma espl
                                                                   gli elementi*/
-    int a[10] = {1, 5, 3};/*qui gli spazzi non usati vengo riempiti con zeri*/
-    
-    /*Il nome di un array è un puntatore costante (non può essere modificato!) 
-    il cui valore corrisponde all'indirizzo del primo elemento del'array*/
-    int n[16] = {}; /*definisco un array di 16 zeri*/
+	int a[10] = {1, 5, 3};/*qui gli spazzi non usati vengo riempiti con zeri*/
 
-    printf("n= %p\n", n);
-    printf("&n[0]= %p\n", &n[0]);
-    
-    printf("*n= %d\n", *n);
-    printf("n[0]= %d\n", n[0]);
+	/*Il nome di un array C( un puntatore costante (non puC2 essere modificato!)
+	il cui valore corrisponde all'indirizzo del primo elemento del'array*/
+	int n[16] = {}; /*definisco un array di 16 zeri*/
+
+	printf("n= %p\n", n);
+	printf("&n[0]= %p\n", &n[0]);
+
+	printf("*n= %d\n", *n);
+	printf("n[0]= %d\n", n[0]);
+
+	/* MACRO UTILI */
+	/*nb Il nome di un array C( un puntatore costante (non puC2 essere modificato!)
+	il cui valore corrisponde all'indirizzo del primo elemento del'array*/
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x)) /* calcolo qunti elementi arrey*/
+	int v[10];
+	int dim = ARRAY_SIZE(v);
+	printf("dimensione array v (non dipende dal tipo di dato) %d\n ",dim);
 }
