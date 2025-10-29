07d3a0c7fd490344019b26df5d9495b413e97463
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-10-29 12:04:01 +0100

    Modify array initialization and add comments
    Updated array definitions and added comments for clarity.

--- array.c
+++ array.c
@@ -6,7 +6,15 @@ int main()
     double numbers[50]; /*posso definirli con tipo nome [cardinalità]*/
     int dxm[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /*non indico cardinalità ma espl
                                                                   gli elementi*/
-    int n[10] = {1, 5, 3};/*qui gli spazzi non usati vengo riempiti con zeri*/
+    int a[10] = {1, 5, 3};/*qui gli spazzi non usati vengo riempiti con zeri*/
     
+    /*Il nome di un array è un puntatore costante (non può essere modificato!) 
+    il cui valore corrisponde all'indirizzo del primo elemento del'array*/
+    int n[16] = {}; /*definisco un array di 16 zeri*/
+
+    printf("n= %p\n", n);
+    printf("&n[0]= %p\n", &n[0]);
     
+    printf("*n= %d\n", *n);
+    printf("n[0]= %d\n", n[0]);
 }
