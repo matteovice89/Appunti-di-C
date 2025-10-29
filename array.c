79ab55bf97ba2fed52d619a00fc9639bd41f350e
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-10-29 11:30:54 +0100

    Define arrays in array.c
    Initialize arrays for numbers and month days.

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    /*definizione*/
+    double numbers[50]; /*posso definirli con tipo nome [cardinalità]*/
+    int dxm[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /*non indico cardinalità ma espl
+                                                                  gli elementi*/
+    int n[10] = {1, 5, 3};/*qui gli spazzi non usati vengo riempiti con zeri*/
+    
+    
+}
