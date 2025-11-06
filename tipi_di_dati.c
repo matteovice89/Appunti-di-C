6139d9feb00a3641087c8cf25c9b321b7acdb6a6
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-11-06 11:34:04 +0100

    Update tipi_di_dati.c

--- tipi_di_dati.c
+++ tipi_di_dati.c
@@ -14,5 +14,15 @@ Il c ammette 5 tipi di dati con modificatori che permettono di estendere
 int main()
 {
     char ch = 'n';
-    printf("ch: %c\t size:%d\n",ch,sizeof(ch));
+    printf("ch: %c\t size:%d\n",ch,sizeof(ch)); //ch: n    size:1
+    
+    int  n = 12;
+    printf("n: %d\t size:%d\n",n,sizeof(n)); //n: 12    size:4
+    
+    float f = 12.5;
+    printf("f: %f\t size:%d\n",f,sizeof(f)); //f: 12.500000     size:4
+    
+    double dl = 23.5681;
+    printf("dl: %f\t size:%d\n",dl,sizeof(dl)); //dl: 23.568100    size:8
 }
+
