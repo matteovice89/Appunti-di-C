cf24633ce59e632ba04e8929d192f77e6cfc28df
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-11-06 11:44:51 +0100

    Update tipi_di_dati.c

--- tipi_di_dati.c
+++ tipi_di_dati.c
@@ -11,18 +11,26 @@ Il c ammette 5 tipi di dati con modificatori che permettono di estendere
  
 #include <stdio.h>
 
+#define PI_GRECO 3.14 //compilatore cerca tutti i punti in cui c'è PI_GRECO e lo sost con 3.14
 int main()
 {
     char ch = 'n';
-    printf("ch: %c\t size:%d\n",ch,sizeof(ch)); //ch: n    size:1
+    printf("ch: %c\t size:%d\n",ch,sizeof(ch));
     
     int  n = 12;
-    printf("n: %d\t size:%d\n",n,sizeof(n)); //n: 12    size:4
+    printf("n: %d\t size:%d\n",n,sizeof(n));
     
     float f = 12.5;
-    printf("f: %f\t size:%d\n",f,sizeof(f)); //f: 12.500000     size:4
+    printf("f: %f\t size:%d\n",f,sizeof(f));
     
     double dl = 23.5681;
-    printf("dl: %f\t size:%d\n",dl,sizeof(dl)); //dl: 23.568100    size:8
+    printf("dl: %f\t size:%d\n",dl,sizeof(dl));
+    
+    const  float pi = 3.14; // la costante non può essere modificata - posso usare una direttiva anche vedi sopra 
+    
+    float nepero = 27.56F; //con f forzo ad avere un valore con virgola
+    
+    //0-9 ABCDEF NUMERI ESADECIMALI
+    int addr = 0x12FA; //CON 0X DAVANTI DIVENTA UN ESADECIMALE
+    printf("addr: %d\n", addr); //così stampo il numero convertito
 }
-
