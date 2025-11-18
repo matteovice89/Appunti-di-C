0554f28217f76a091fc530268fc3f5d160b7755b
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-11-18 12:01:49 +0100

    Refactor pointer initialization and add comments
    Initialized pointer 'p' at declaration and added comments for clarity. Modified the value pointed to by 'p' and introduced a second pointer 'p2' to demonstrate pointer assignment.

--- 06Puntatori.c
+++ 06Puntatori.c
@@ -10,9 +10,20 @@ SONO VARIABILI CHE MANTENGONO LA POSIZINE*/
 int main()
 {
     int x=5;
-    int *p; //dichiaro il tipo e con * prima del nome 
-    p= &x; //con & assegno a p l'indirizzo di memoria della variabile x
-    printf("%d",*p); //con *p vado a restituire il valore della var a cui punta il puntatore p
+    int *p = &x; //dichiaro il tipo e con * prima del nome (buona cosa inizializzarlo subito)
+    //p= &x; con & assegno a p l'indirizzo di memoria della variabile x - 
+    printf("adr: %p\n",p); // con %p stampo contenuto puntatore
+    printf("valore: %d\n",*p); //con *p vado a restituire il valore della var a cui punta il puntatore p
+    
+    (*p)=567; //vado a modificare direttamtne il valore a cui punta il puntatore
+    printf("dopo il riassegnamenteo:\nvariabile: %d\nvalore a cui punta il puntatore:%d\n",x,*p);
+// quindi posso intervenire sulla variabile modificando direttametne il valore a cui punta il puntatore
 
+    int *p2;
+    p2=p;
+    printf("adr: %p\n",p2);
+    printf("valore: %d\n",*p2);
+    //copio giusto giusto quant dal secondo puntatore
+    
     return 0;
 }
